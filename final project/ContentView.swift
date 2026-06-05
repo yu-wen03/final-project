c7d2e283d1e83e710c6fa4582f8d193f58395bcf
yuwen <sm013924@gmail.com>
2026-06-05 13:52:48 +0800

    Initial Commit

---
+++ final project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  final project
+//
+//  Created by 114-2Student04 on 2026/6/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ final project/final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_projectApp.swift
+//  final project
+//
+//  Created by 114-2Student04 on 2026/6/5.
+//
+
+import SwiftUI
+
+@main
+struct final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
